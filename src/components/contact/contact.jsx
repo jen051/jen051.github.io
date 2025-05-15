93c3f8b91ed60a7f6ffbfe2f91d7895f5fe733df
jen051 <jenniferjiang0501@gmail.com>
2025-05-14 23:01:15 -0600

    functional contact form

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -2,13 +2,38 @@ import React from 'react'
 import './contact.css'
 
 const Contact = () => {
+    const [result, setResult] = React.useState("");
+
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        setResult("Sending....");
+        const formData = new FormData(event.target);
+
+        formData.append("access_key", "948c5bd2-9f01-4d3d-a83a-40238f680070");
+
+        const response = await fetch("https://api.web3forms.com/submit", {
+            method: "POST",
+            body: formData
+        });
+
+        const data = await response.json();
+
+        if (data.success) {
+            setResult("Form Submitted Successfully");
+            event.target.reset();
+        } else {
+            console.log("Error", data);
+            setResult(data.message);
+        }
+    };
+    
     return (
         <div id='contact' className='contact'>
             <div className="contact-desc">
                 <h1>Let's Talk!</h1>
                 <p>I'm currently open to work as well as new projects. Feel free to reach out any time!</p>
             </div>
-            <form className='contact-form'>
+            <form className='contact-form' onSubmit={onSubmit}>
                 <label htmlFor="">Your Name</label>
                 <input type="text" placeholder='Enter your name' name='name' />
                 <label htmlFor="">Your Email</label>
